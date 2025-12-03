74aa94fe827c3bcd1d9cbf18dca51f8dfa3dd7f4
xdrshjr <476494220@qq.com>
2025-12-04 00:29:29 +0800

    [init] project init.

---
+++ train/src/train.py
@@ -0,0 +1,13 @@
+from llmtuner.train.tuner import run_exp
+
+def main():
+    # wandb.login()
+    run_exp()
+
+def _mp_fn(index):
+    # For xla_spawn (TPUs)
+    run_exp()
+
+
+if __name__ == "__main__":
+    main()
